3bc67a49a25bf7484bc665a1865fb3e255e2ba9c
luhc228 <luhengchang228@126.com>
2023-04-22 15:20:09 +0800

    chore: update comment

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 // 方式0：使用 lodash 模块
-import _ from 'lodash';
-const merge = _.merge;
-const mergeWith = _.mergeWith;
+// import _ from 'lodash';
+// const { merge, mergeWith } = _;
 
-// 方式1：使用子模块
 // import merge from 'lodash.merge';
 // import mergeWith from 'lodash.mergewith';
 
-// 方式2：使用 lodash subpath 子模块
 // import merge from 'lodash/merge';
 // import mergeWith from 'lodash/mergewith';
 
 // 方式3：使用 lodash-es
-// import { mergeWith, merge } from 'lodash-es';
+import { mergeWith, merge } from 'lodash-es';
 
 var object = {
   'a': [{ 'b': 2 }, { 'd': 4 }]
@@ -28,5 +25,7 @@ function customizer(objValue, srcValue) {
   }
 }
 
+// console.log(_.merge(object, other));
+// console.log(_.mergeWith(object, other, customizer));
 console.log(merge(object, other));
 console.log(mergeWith(object, other, customizer));
